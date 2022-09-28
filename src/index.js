5af167aabbc60a426100a2cf9fa3009c7596a721
MightyDarkGlasses <cantoria.alexis.p.5055@gmail.com>
2022-09-28 16:16:26 +0800

    Firebase Storage implementation (Vehicle)

--- src/index.js
+++ src/index.js
@@ -61,8 +61,10 @@ console.log(auth);
 /************** == LOGIN PAGE == ***************/
 /************** == LOGIN PAGE == ***************/
 const loginForm = document.querySelector('.login'); 
-
-
+// let allImageLinks = []; //all of the images links are stored here to be uploaded ng Firebase
+let imageLinksFront = "";
+let imageLinksSide = "";
+let imageLinksRear = "";
 
 // console.log(loginForm)
 if(loginForm !== undefined && loginForm !== null) {
@@ -118,6 +120,7 @@ function checkCurrentLoggedUser() {
         else {
             console.log("Email is not yet verified.");
             alert("Please verify your e-mail first.")
+            // sendVerification();
         }
     }
     return;
@@ -168,25 +171,36 @@ function getCurrentLoggedUserUID() {
     const user = auth.currentUser;
     return user.uid;
 }
+
 let registerButtonFinal = document.getElementById("reg-goto-final");
 if (registerButtonFinal !== null && registerButtonFinal !== undefined) {
-
     
     registerButtonFinal.addEventListener("click", () => {
+        // console.log(createVehicleImageData("RANDOMUSERID")); //dito ka nga
         createUserWithEmailAndPassword(auth, getCookie("email"), getCookie("pass"))
         .then((cred) => {
             console.log('User created: ', cred.user);
             const userId = getCurrentLoggedUserUID();
+            console.log(userId);
             createNewData(userId);
             // createVehicleImageData(userId);
         }).catch((err) => {
             console.log("Signup error message: ", err); //e.g password is wrong or too short, invalid email, etc.
         });
 
+        
+        // generateVehicleQRCode("Testing", 256);
+        // const getQRCode = document.getElementById("qrcode");
+        // console.log(getQRCode.firstElementChild);
+        // console.log(getQRCode.attributes[1]);
         // createVehicleImageData("THISISUSERIDHAHAHAHA");
     });
 }
-function createNewData(userUID) {
+async function createNewData(userUID) {
+    let allOfMyLinks = await createVehicleImageData(userUID);
+    console.log("allOfMyLinks", allOfMyLinks)
+
+    //Account Information (Firestore)
     setDoc(doc(db, "account-information", userUID), {
         id_number: getCookie("id"),
         first_name: getCookie("fname"),
@@ -198,11 +212,44 @@ function createNewData(userUID) {
     }).then(() => {
         console.log("Account Information was added in the collection");
     });
+
+    // let image1 = imagesLinks[0];
+    // let image2 = imagesLinks[1];
+    // let image3 = imagesLinks[2];
+
+    // console.log("Contents: ", image1, image2, image3);
+    // console.log("Content TypeOf: ", typeof(image1), typeof(image2), typeof(image3));
+    
+    // console.log("allImageLinks", allImageLinks);
+    // console.log("allImageLinks-typeOf:", allImageLinks);
+    // console.log("allImageLinks[0]", allImageLinks[0]);
+    // console.log("allImageLinks[1]", allImageLinks[1]);
+    // console.log("allImageLinks[2]", allImageLinks[2]);
+    // console.log("imageLinksFront:", imageLinksFront)
+    // console.log("imageLinksSide:", imageLinksSide)
+    // console.log("imageLinksRear:", imageLinksRear)
+
+    // console.log("createVehicleImageData(userUID):", createVehicleImageData(userUID));
+    // console.log("createVehicleImageData(userUID)_typeof:", typeof(createVehicleImageData(userUID)));
+    // console.log("typeofDummyArray:", typeof(["", "", ""]))
+    
+    // console.log("ArrayCheckKungNakuhaBababababhabsfhabsfbjadsfhbjb", links);
+
+    
+
+    //Vehicle Information (Firestore)
     setDoc(doc(db, "vehicle-information", userUID), {
         registered_vehicle: {
             vehicles: {
-                // images: ["front", "side", "rear"]
-                images: createVehicleImageData(userID) //add Vehicle, store the vehicle on the fly.
+                // images: [allImageLinks(0), allImageLinks(1), allImageLinks(2)]
+                // images:  [imageLinks[0], imageLinks[1], imageLinks[2]]//add Vehicle, store the vehicle on the fly.
+                // images: createVehicleImageData(userUID)
+                images: allOfMyLinks
+                // images: {
+                //     "front": links[0],
+                //     "side": links[1],
+                //     "rear": links[2]
+                // }
             },
             qrcode: ["QRCODE-TEST"],
             plate: [getCookie("plate")],
@@ -211,7 +258,7 @@ function createNewData(userUID) {
         createdAt: serverTimestamp()
     }).then(() => {
         console.log("Vehicle Information was added in the collection");
-
+        
         //Success! Redirect to the next page.
         // sendVerification();
         // window.location = "signup4.html";
@@ -219,24 +266,26 @@ function createNewData(userUID) {
 }
 
 function createVehicleImageData(userId) {
-    // vehicle-front
-    // vehicle-side
-    // vehicle-rear
+    let imageLinks = []; //STORE THE VEHICLE IMAGE LINKS HERE...
+    // // vehicle-front
+    // // vehicle-side
+    // // vehicle-rear
 
-    // vehicle-front-filename
-    // vehicle-side-filename
-    // vehicle-rear-filename
+    // // vehicle-front-filename
+    // // vehicle-side-filename
+    // // vehicle-rear-filename
 
-    // vehicle-front-filetype
-    // vehicle-side-filetype
-    // vehicle-rear-filetype
-
-    // let vehicle_front = [localStorage.getItem("vehicle-front").replace(/^data:image\/(png|jpeg);base64,/, ""), 
-    // localStorage.getItem("vehicle-front-filename"), localStorage.getItem("vehicle-front-filetype")];
-    // let vehicle_side = [localStorage.getItem("vehicle-side").replace(/^data:image\/(png|jpeg);base64,/, ""), 
-    // localStorage.getItem("vehicle-side-filename"), localStorage.getItem("vehicle-side-filetype")];
-    // let vehicle_rear = [localStorage.getItem("vehicle-rear").replace(/^data:image\/(png|jpeg);base64,/, ""), 
-    // localStorage.getItem("vehicle-rear-filename"), localStorage.getItem("vehicle-rear-filetype")];\\\
+  
+    // // vehicle-front-filetype
+    // // vehicle-side-filetype
+    // // vehicle-rear-filetype
+  
+    // // let vehicle_front = [localStorage.getItem("vehicle-front").replace(/^data:image\/(png|jpeg);base64,/, ""), 
+    // // localStorage.getItem("vehicle-front-filename"), localStorage.getItem("vehicle-front-filetype")];
+    // // let vehicle_side = [localStorage.getItem("vehicle-side").replace(/^data:image\/(png|jpeg);base64,/, ""), 
+    // // localStorage.getItem("vehicle-side-filename"), localStorage.getItem("vehicle-side-filetype")];
+    // // let vehicle_rear = [localStorage.getItem("vehicle-rear").replace(/^data:image\/(png|jpeg);base64,/, ""), 
+    // // localStorage.getItem("vehicle-rear-filename"), localStorage.getItem("vehicle-rear-filetype")];\\\
 
 
     let fileNameFront = localStorage.getItem("vehicle-front-filename");
@@ -254,11 +303,6 @@ function createVehicleImageData(userId) {
     blobVehicleFront = base64ToBlob(vehicle_front[0], vehicle_front[2]);  
     blobVehicleSide = base64ToBlob(vehicle_side[0], vehicle_side[2]);  
     blobVehicleRear = base64ToBlob(vehicle_rear[0], vehicle_rear[2]);  
-    
-
-
-    
-    // console.log(localStorage.getItem("vehicle-rear-filename"), fileName.substring(fileName.lastIndexOf(".")))
 
     // console.log(vehicle_front);
     // console.log(vehicle_side);
@@ -268,20 +312,23 @@ function createVehicleImageData(userId) {
     // console.log(blobVehicleSide);
     // console.log(blobVehicleRear);
 
-    let imageLinks = [];
+    
     const metadataFront = { contentType: vehicle_front[2], };
     const metadataSide = { contentType: vehicle_side[2], };
     const metadataRear = { contentType: vehicle_rear[2], };
 
-    const storageRef1 = ref(storage, `vehicle-information/${userId}/${vehicle_front[1]}`);
-    const storageRef2 = ref(storage, `vehicle-information/${userId}/${vehicle_side[1]}`);
-    const storageRef3 = ref(storage, `vehicle-information/${userId}/${vehicle_rear[1]}`);
+    const storageRef1 = ref(storage, `vehicle-information/${userId}/1/${vehicle_front[1]}`);
+    const storageRef2 = ref(storage, `vehicle-information/${userId}/1/${vehicle_side[1]}`);
+    const storageRef3 = ref(storage, `vehicle-information/${userId}/1/${vehicle_rear[1]}`);
     
     const uploadTask1 = uploadBytesResumable(storageRef1, blobVehicleFront, metadataFront);
     const uploadTask2 = uploadBytesResumable(storageRef2, blobVehicleSide, metadataSide);
     const uploadTask3 = uploadBytesResumable(storageRef3, blobVehicleRear, metadataRear);
 
-    uploadTask1.on('state_changed', (snapshot) => {
+    // Upload image (front view)
+    // link1 = link2 = link3 = "";
+     uploadTask1.on('state_changed', (snapshot) => {
+            // Progress of fileupload
             const progress = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
             console.log("Uploading vehicle front.");
             console.log('Upload is ' + progress + '% done');    //progress of upload
@@ -291,15 +338,20 @@ function createVehicleImageData(userId) {
             console.log(error);
         }, 
         () => {
+            // If successful, do this.
             getDownloadURL(uploadTask1.snapshot.ref).then((downloadURL) => {
                 console.log('File available at', downloadURL);
                 console.log(typeof(downloadURL))
-                imageLinks.push(downloadURL);
+                imageLinks.push(downloadURL); //append the user link (vehicle)
+                // link1 = `Testing1${downloadURL}`;
+                // allImageLinks.push(downloadURL);
+                // imageLinksFront = "" + downloadURL;
+                // console.log("URLLocationimageLinksFront", imageLinksFront);
             });
         } //end of getDownloadURL
     ); //end of on method
 
-    uploadTask2.on('state_changed', (snapshot) => {
+     uploadTask2.on('state_changed', (snapshot) => {
             const progress = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
             console.log("Uploading vehicle side.");
             console.log('Upload is ' + progress + '% done');    //progress of upload
@@ -313,11 +365,15 @@ function createVehicleImageData(userId) {
                 console.log('File available at', downloadURL);
                 console.log(typeof(downloadURL))
                 imageLinks.push(downloadURL);
+                // link2 = `Testing2${downloadURL}`;
+                // allImageLinks.push(downloadURL);
+                // imageLinksSide = "" + downloadURL;
+                // console.log("URLLocationimageLinksFront", imageLinksFront);
             });
         } //end of getDownloadURL
     ); //end of on method
 
-    uploadTask3.on('state_changed', (snapshot) => {
+     uploadTask3.on('state_changed', (snapshot) => {
             const progress = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
             console.log("Uploading vehicle rear.");
             console.log('Upload is ' + progress + '% done');    //progress of upload 
@@ -331,19 +387,38 @@ function createVehicleImageData(userId) {
                 console.log('File available at', downloadURL);
                 console.log(typeof(downloadURL))
                 imageLinks.push(downloadURL);
+                // link3 = `Testing3${downloadURL}`;
+                // allImageLinks.push(downloadURL);
+                // imageLinksRear = "" + downloadURL;
             });
         } //end of getDownloadURL
     ); //end of on method
 
-
+    // imageLinks.push(link1);
+    // imageLinks.push(link2);
+    // imageLinks.push(link3);
     //vehicle_front[1]
     //vehicle_side[1]
     //vehicle_rear[1]
 
-    return imageLinks;
+    console.log("BeforeReturn_Image Links: ", imageLinks);
+    return imageLinks; //return the image link of the provided image
 }
 
 
+function generateVehicleQRCode(myText, mySize) {
+    const generateQRCode = (text, size) => {
+        const qrcode = new QRCode('qrcode', {
+            text: text,
+            width: size,
+            height: size,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        })
+    };
+    generateQRCode(myText, mySize);
+}
 // Convert base64 (generated by FileReader) into Blob (which is supported by Firebase Storage)
 function base64ToBlob(base64, mime) 
 {
